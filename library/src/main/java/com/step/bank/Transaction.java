8e230d60c87f6dbdc90299fb209b6f73e2869f6e
suyog <suyogukalkar3@gmail.com>
2018-03-26 08:32:45 +0530

    * Added transaction class for create transaction.

--- library/src/main/java/com/step/bank/Transaction.java
+++ library/src/main/java/com/step/bank/Transaction.java
@@ -1,9 +1,27 @@
 package com.step.bank;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Transaction {
     private final Date date;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return Double.compare(that.balance, balance) == 0 &&
+                Objects.equals(date, that.date) &&
+                Objects.equals(person, that.person);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(date, person, balance);
+    }
+
     private final String person;
     private final double balance;
 
